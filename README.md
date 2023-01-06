9750e5f7ff7e7840423461e0f24c70ab164b78d6
nalaadina <inter.dev@nexagroup.co.id>
2023-01-06 21:08:41 +0700

    Mungkin komit pertama dan terakhir

---
+++ README.md
@@ -0,0 +1,8 @@
+# Naive Bayes - Genetic Algoritm using Django
+
+install semua requirement pada requirement.txt
+
+run diterminal > python manage.py runserver
+
+buka browser ketik : 127.0.0.1:8000
+
---
+++ administrator/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, include
+from . import views
+
+app_name = 'administrator'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('dataset/', views.dataset, name='dataset'),
+    path('tentang/', views.tentang, name='tentang'),
+    path('EDA/', views.EDA, name='EDA'),
+    path('encode/', views.encode, name='encode'),
+    path('split_data_chi2/', views.SplitData, name='split_data_chi2'),
+    #path('klasifikasi/', views.klasifikasi, name='klasifikasi'),
+    path('Catboost/', views.Catboost, name='Catboost'),
+    path('Catboost_Chi2/', views.Catboost_Chi2, name='Catboost_Chi2'),
+    path('hasil_catboost_chi2/', views.hasil_catboost_chi2, name='hasil_catboost_chi2'),
+    #path('hasilsvmrbf/', views.hasilsvmrbf, name='hasilsvmrbf'),
+    #path('SVMRBFIG/', views.SVMRBFIG, name='SVMRBFIG'),
+
+
+]
